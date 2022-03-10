d5751916b4ed735e513565a0f7fca5606bee747c
Sem <sem.nurmatov@gmail.com>
2022-03-10 23:05:04 +0300

    fix: fixed and added texts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
+import express from 'express';
+
 import TelegramBot from "node-telegram-bot-api";
-import {basicPlan, courseDocsId, greetingsText, proPlan, startText, submission} from "./ITextData";
+import {basicPlan, courseDocsId, greetingsText, noCaptionFile, proPlan, startText, submission, submitted} from "./ITextData";
 
 require('dotenv').config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
 
 const options = {
     reply_markup: {
@@ -15,22 +26,44 @@ const bot = new TelegramBot(process.env.MAFA_BOT_TOKEN, { polling: true});
 bot.on('message', async (mes, metadata?) => {
     console.log("Message: " , mes);
     const chatId = mes.chat.id;
-    const caption = mes.caption;
 
     if(metadata.type !== 'text'){
         console.log('----' + metadata.type);
         const type = metadata.type;
+        let username;
         let fileId;
         let sendFile;
+        let sendMessage;
+        let caption;
 
         switch (type) {
             case 'photo':
-                fileId = mes.photo[mes.photo.length-1].file_id;
-                sendFile = await bot.sendPhoto(courseDocsId, fileId, {caption: caption});
+                if(mes.caption === undefined){
+                    console.log('22222 ' + mes.caption);
+                    
+                    await bot.sendMessage(chatId, noCaptionFile, { parse_mode: "Markdown"});
+                }else{
+                    fileId = mes.photo[mes.photo.length-1].file_id;
+                    username = 'Sender username:  @' + mes.chat.username;
+                    sendFile = await bot.sendPhoto(courseDocsId, fileId, {caption: caption});
+                    sendMessage = await bot.sendMessage(courseDocsId, username);
+                    await bot.sendMessage(chatId, submitted);
+
+                }
                 break;
             case 'document':
-                fileId = mes.document.file_id;
+                if(mes.caption === undefined){
+                    console.log('11111 ' + mes.caption);
+
+                    await bot.sendMessage(chatId, noCaptionFile, { parse_mode: "Markdown"});
+                }else{
+                    fileId = mes.document.file_id;
+                username = 'Sender username: @' + mes.chat.username;
                 sendFile = await bot.sendDocument(courseDocsId, fileId, {caption: caption});
+                sendMessage = await bot.sendMessage(courseDocsId, username);
+                await bot.sendMessage(chatId, submitted);
+
+                }
                 break;
         }
 
@@ -39,7 +72,7 @@ bot.on('message', async (mes, metadata?) => {
 
 
         console.log("courseDocsId : ", courseDocsId);
-        await bot.sendMessage(chatId, sendFile.caption);
+        // await bot.sendMessage(chatId, submitted);
     }else{
         await bot.sendMessage(chatId, greetingsText, options);
     }
@@ -97,6 +130,11 @@ bot.on('callback_query', async function onCallbackQuery(cbQuery){
 
     await bot.editMessageText(text, opts);
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
+
 
 /*
 bot.sendMessage(mes.chat.id, "Welcome", {
